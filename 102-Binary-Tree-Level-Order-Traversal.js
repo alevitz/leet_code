d511ca93299f34ab2fccf1f58cb19ae1b81da623
Adam <adamlevitz@gmail.com>
2021-06-21 22:13:15 -0700

    iterative solution update

--- 102-Binary-Tree-Level-Order-Traversal.js
+++ 102-Binary-Tree-Level-Order-Traversal.js
@@ -1,26 +1,50 @@
+// var levelOrder = function(root) {
+    
+//     let orderArr = []; 
+    
+//     function order(node, depth = 0){
+//         if(!node){    
+//         return;
+//         }
+        
+//         if(!orderArr[depth]){
+//         orderArr[depth] = [];
+//         }
+//         orderArr[depth].push(node.val);
+//         depth++;        
+//         if(node.left){
+//             order(node.left, depth);
+//         }
+//         if(node.right){
+//             order(node.right, depth);
+//         }               
+//     }
+    
+//     order(root);
+    
+//     return orderArr;
+// };
+
 var levelOrder = function(root) {
     
-    let orderArr = []; 
+    if(!root)return [];
     
-    function order(node, depth = 0){
-        if(!node){    
-        return;
-        }
-        
-        if(!orderArr[depth]){
-        orderArr[depth] = [];
-        }
-        orderArr[depth].push(node.val);
-        depth++;        
-        if(node.left){
-            order(node.left, depth);
-        }
-        if(node.right){
-            order(node.right, depth);
-        }               
-    }
+   let result = [];
+    let queue = [];
     
-    order(root);
+    queue.push(root);
     
-    return orderArr;
+    while(queue.length){
+        let currentLevelValues =[];
+        let length = queue.length, count = 0;
+        while(count < length){
+            let current = queue.shift();
+        currentLevelValues.push(current.val);
+        if(current.left)queue.push(current.left);
+        if(current.right)queue.push(current.right);
+            count++;
+        } 
+         result.push(currentLevelValues);      
+    }       
+    return result;
 };
