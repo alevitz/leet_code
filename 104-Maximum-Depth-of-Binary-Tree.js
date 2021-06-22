219148cdd052389cec3ab29c4f42f2f5dc858219
Adam <adamlevitz@gmail.com>
2021-06-21 18:51:16 -0700

    problem solved in less steps

--- 104-Maximum-Depth-of-Binary-Tree.js
+++ 104-Maximum-Depth-of-Binary-Tree.js
@@ -1,25 +1,34 @@
-var maxDepth = function(root) {
+// var maxDepth = function(root) {
     
-  if(!root){
-      return 0;
-  }
+//   if(!root){
+//       return 0;
+//   }
         
-     function deepest(node, depth=1){
-         let left = depth;
-         let right = depth;
-         let leftResult = depth;
-         let rightResult = depth;
+//      function deepest(node, depth=1){
+//          let left = depth;
+//          let right = depth;
+//          let leftResult = depth;
+//          let rightResult = depth;
          
-         if(node.left){
-             left++;
-            leftResult = deepest(node.left,left);
-         }
-         if(node.right){
-             right++;
-             rightResult = deepest(node.right,right);
-         }
-             return Math.max(leftResult, rightResult);
-         }           
+//          if(node.left){
+//              left++;
+//             leftResult = deepest(node.left,left);
+//          }
+//          if(node.right){
+//              right++;
+//              rightResult = deepest(node.right,right);
+//          }
+//              return Math.max(leftResult, rightResult);
+//          }           
      
-     return deepest(root);
- }
+//      return deepest(root);
+//  }
+
+var maxDepth = function(root, currentLevel=0) {
+    if(!root)return currentLevel;
+    
+    currentLevel++;
+    return Math.max(maxDepth(root.left, currentLevel),
+    maxDepth(root.right, currentLevel));
+    
+};
